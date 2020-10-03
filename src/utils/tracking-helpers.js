1bc5ce576dec4590c35471d015e1a9453cca17f6
Gabriel Oliveira <gabriel@yampi.com.br>
2020-10-03 18:20:21 -0300

    eslint(all) Apply eslint fixes to all files

--- src/utils/tracking-helpers.js
+++ src/utils/tracking-helpers.js
@@ -1,22 +1,22 @@
 const _isString = require('lodash/isString');
-const { VALID_INITIALS } = require('./consts')
+const { VALID_INITIALS } = require('./consts');
 
 function matchesPattern (object) {
-    return /^([A-Z]{2}[0-9]{9}[A-Z]{2}){1}$/.test(object)
+    return /^([A-Z]{2}[0-9]{9}[A-Z]{2}){1}$/.test(object);
 }
 
-function category(object) {
-    if ( !matchesPattern(object) || !_isString(object) ) {
-        return
+function category (object) {
+    if (! matchesPattern(object) || ! _isString(object)) {
+        return;
     }
 
-    return VALID_INITIALS[object.substr(0, 2)]
+    return VALID_INITIALS[object.substr(0, 2)];
 }
 
 function isValid (object) {
-    return category(object) !== undefined
+    return category(object) !== undefined;
 }
 
 module.exports = {
-    category, isValid
-}
+    category, isValid,
+};
